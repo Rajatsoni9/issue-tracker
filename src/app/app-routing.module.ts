816a460753528f4630128648d77bfda2029dc37b
Rajat Soni <rajatsoni9@outlook.com>
2021-08-29 16:08:38 +0530

    refactor: add lazy loading for issues and kanban view

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,15 @@ import { Routes, RouterModule } from '@angular/router';
 import { IssuesComponent } from './issues/issues.component';
 
 const routes: Routes = [
-  { path: 'issues', component: IssuesComponent },
+  {
+    path: 'issues',
+    component: IssuesComponent,
+    children: [
+      { path: 'kanban', loadChildren: () => import('./kanban/kanban.module').then(m => m.KanbanModule) },
+      { path: 'list', loadChildren: () => import('./issue-list/issue-list.module').then(m => m.IssueListModule) },
+      { path: '', pathMatch: 'full', redirectTo: 'kanban' }
+    ]
+  },
   { path: '', pathMatch: 'full', redirectTo: '/issues' },
   { path: '**', redirectTo: '/issues' },
 ];
@@ -12,4 +20,4 @@ const routes: Routes = [
   imports: [RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' })],
   exports: [RouterModule],
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
--- src/app/issues/issues.component.ts
+++ src/app/issues/issues.component.ts
@@ -1,18 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
-import { Issue } from '../app.interfaces';
 import { CreateIssueComponent } from '../create-issue/create-issue.component';
-import { IssueService } from '../issue.service';
 
 @Component({
   selector: 'app-issues',
   templateUrl: './issues.component.html',
   styleUrls: ['./issues.component.scss'],
 })
-export class IssuesComponent implements OnInit {
+export class IssuesComponent {
   constructor(private dialog: MatDialog) {}
-
-  ngOnInit(): void {}
 
   createIssue(): void {
     this.dialog.open(CreateIssueComponent, { width: '80vw' });
